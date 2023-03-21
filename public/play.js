38b0b2f7a1eee74a1bf0850678cf0405866859a5
bkdredge <74231210+bkdredge@users.noreply.github.com>
2023-03-20 19:56:46 -0600

    update

--- public/play.js
+++ public/play.js
@@ -1,190 +1,70 @@
-const btnDescriptions = [
-  { file: 'sound1.mp3', hue: 120 },
-  { file: 'sound2.mp3', hue: 0 },
-  { file: 'sound3.mp3', hue: 60 },
-  { file: 'sound4.mp3', hue: 240 },
-];
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Simon</title>
 
-class Button {
-  constructor(description, el) {
-    this.el = el;
-    this.hue = description.hue;
-    this.sound = loadSound(description.file);
-    this.paint(25);
-  }
+    <link rel="stylesheet" href="main.css" />
 
-  paint(level) {
-    const background = `hsl(${this.hue}, 100%, ${level}%)`;
-    this.el.style.backgroundColor = background;
-  }
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body class="bg-dark text-light">
+    <header class="container-fluid">
+      <nav class="navbar fixed-top navbar-dark">
+        <a class="navbar-brand" href="#">Simon<sup>&reg;</sup></a>
+        <menu class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="play.html">Play</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="scores.html">Scores</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About</a>
+          </li>
+        </menu>
+      </nav>
+    </header>
 
-  async press(volume) {
-    this.paint(50);
-    await this.play(volume);
-    this.paint(25);
-  }
+    <main class="bg-secondary">
+      <div class="players">
+        Player:
+        <span class="player-name"></span>
+      </div>
+      <div class="game">
+        <div class="button-container">
+          <!-- buttons are discovered in the JavaScript by selecting the game-button class -->
+          <!-- onclick triggers a button push interaction -->
+          <button id="green" class="game-button button-top-left" onclick="game.pressButton(this)"></button>
+          <button id="red" class="game-button button-top-right" onclick="game.pressButton(this)"></button>
+          <button id="yellow" class="game-button button-bottom-left" onclick="game.pressButton(this)"></button>
+          <button id="blue" class="game-button button-bottom-right" onclick="game.pressButton(this)"></button>
+          <div class="controls center">
+            <div class="game-name">Simon<sup>&reg;</sup></div>
+            <div id="score" class="score center">--</div>
+            <button class="btn btn-primary" onclick="game.reset()">Reset</button>
+          </div>
+        </div>
+      </div>
+    </main>
 
-  // Work around Safari's rule to only play sounds if given permission.
-  async play(volume = 1.0) {
-    this.sound.volume = volume;
-    await new Promise((resolve) => {
-      this.sound.onended = resolve;
-      this.sound.play();
-    });
-  }
-}
+    <footer class="bg-dark text-dark text-muted">
+      <div class="container-fluid">
+        <span class="text-reset">Author Name(s)</span>
+        <a class="text-reset" href="https://github.com/bkdredge/simon">Source</a>
+      </div>
+    </footer>
 
-class Game {
-  buttons;
-  allowPlayer;
-  sequence;
-  playerPlaybackPos;
-  mistakeSound;
-
-  constructor() {
-    this.buttons = new Map();
-    this.allowPlayer = false;
-    this.sequence = [];
-    this.playerPlaybackPos = 0;
-    this.mistakeSound = loadSound('error.mp3');
-
-    document.querySelectorAll('.game-button').forEach((el, i) => {
-      if (i < btnDescriptions.length) {
-        this.buttons.set(el.id, new Button(btnDescriptions[i], el));
-      }
-    });
-
-    const playerNameEl = document.querySelector('.player-name');
-    playerNameEl.textContent = this.getPlayerName();
-  }
-
-  async pressButton(button) {
-    if (this.allowPlayer) {
-      this.allowPlayer = false;
-      await this.buttons.get(button.id).press(1.0);
-
-      if (this.sequence[this.playerPlaybackPos].el.id === button.id) {
-        this.playerPlaybackPos++;
-        if (this.playerPlaybackPos === this.sequence.length) {
-          this.playerPlaybackPos = 0;
-          this.addButton();
-          this.updateScore(this.sequence.length - 1);
-          await this.playSequence();
-        }
-        this.allowPlayer = true;
-      } else {
-        this.saveScore(this.sequence.length - 1);
-        this.mistakeSound.play();
-        await this.buttonDance(2);
-      }
-    }
-  }
-
-  async reset() {
-    this.allowPlayer = false;
-    this.playerPlaybackPos = 0;
-    this.sequence = [];
-    this.updateScore('--');
-    await this.buttonDance(1);
-    this.addButton();
-    await this.playSequence();
-    this.allowPlayer = true;
-  }
-
-  getPlayerName() {
-    return localStorage.getItem('userName') ?? 'Mystery player';
-  }
-
-  async playSequence() {
-    await delay(500);
-    for (const btn of this.sequence) {
-      await btn.press(1.0);
-      await delay(100);
-    }
-  }
-
-  addButton() {
-    const btn = this.getRandomButton();
-    this.sequence.push(btn);
-  }
-
-  updateScore(score) {
-    const scoreEl = document.querySelector('#score');
-    scoreEl.textContent = score;
-  }
-
-  async buttonDance(laps = 1) {
-    for (let step = 0; step < laps; step++) {
-      for (const btn of this.buttons.values()) {
-        await btn.press(0.0);
-      }
-    }
-  }
-
-  getRandomButton() {
-    let buttons = Array.from(this.buttons.values());
-    return buttons[Math.floor(Math.random() * this.buttons.size)];
-  }
-
-  async saveScore(score) {
-    const userName = this.getPlayerName();
-    const date = new Date().toLocaleDateString();
-    const newScore = { name: userName, score: score, date: date };
-
-    try {
-      const response = await fetch('/api/score', {
-        method: 'POST',
-        headers: { 'content-type': 'application/json' },
-        body: JSON.stringify(newScore),
-      });
-
-      // Store what the service gave us as the high scores
-      const scores = await response.json();
-      localStorage.setItem('scores', JSON.stringify(scores));
-    } catch {
-      // If there was an error then just track scores locally
-      this.updateScoresLocal(newScore);
-    }
-  }
-
-  updateScoresLocal(newScore) {
-    let scores = [];
-    const scoresText = localStorage.getItem('scores');
-    if (scoresText) {
-      scores = JSON.parse(scoresText);
-    }
-
-    let found = false;
-    for (const [i, prevScore] of scores.entries()) {
-      if (newScore > prevScore.score) {
-        scores.splice(i, 0, newScore);
-        found = true;
-        break;
-      }
-    }
-
-    if (!found) {
-      scores.push(newScore);
-    }
-
-    if (scores.length > 10) {
-      scores.length = 10;
-    }
-
-    localStorage.setItem('scores', JSON.stringify(scores));
-  }
-}
-
-const game = new Game();
-
-function delay(milliseconds) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true);
-    }, milliseconds);
-  });
-}
-
-function loadSound(filename) {
-  return new Audio('assets/' + filename);
-}
+    <!-- Script is located at the bottom because it references HTML elements during initialization -->
+    <script src="play.js"></script>
+  </body>
+</html>
